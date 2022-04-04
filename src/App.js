a95dd21c9c81cc3874076ec321bfbfba3e57416c
Saurabh Kumar <saurabhkr9902@gmail.com>
2022-04-04 15:08:22 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -5,25 +5,7 @@ import PatientDetailPage from './pages/PatientDetailPage';
 
 
 function App() {
-  // const [data, setData] = useState([])
-  // const [file, setFile] = useState([])
 
-  // useEffect(() => {
-  //   axios.get("https://619f39821ac52a0017ba467e.mockapi.io/patientDetails")
-  //     .then((response) => {
-  //       setData(response.data);
-  //     });
-
-  //   console.log("patient detail-",data)
-
-  // }, []);
-  // useEffect(() =>{
-  //   axios.get("https://619f39821ac52a0017ba467e.mockapi.io/Files")
-  //   .then((response) => {
-  //     console.log("file data-",response);
-  //     setFile(response.file);
-  //   });
-  // }, [])
   return (
     <div className='d-flex '>
       <Sidepanel />
